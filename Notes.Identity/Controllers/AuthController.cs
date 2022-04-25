18452e2af51f977a96b2fb7e9e7163705380cc22
kycnexynpuwejl <kycnexynpuwejl@protonmail.com>
2022-04-25 12:46:59 +0300

    Add Register, Login, Logout

--- Notes.Identity/Controllers/AuthController.cs
+++ Notes.Identity/Controllers/AuthController.cs
@@ -1,3 +1,5 @@
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Notes.Identity.Models;
 
@@ -5,6 +7,14 @@ namespace Notes.Identity.Controllers;
 
 public class AuthController : Controller
 {
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly IIdentityServerInteractionService _interactionService;
+    public AuthController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager,
+        IIdentityServerInteractionService interactionService) =>
+        (_signInManager, _userManager, _interactionService) =
+        (signInManager, userManager, interactionService);
+    
     [HttpGet]
     public IActionResult Login(string returnUrl)
     {
@@ -15,8 +25,64 @@ public class AuthController : Controller
         return View(viewModel);
     }
     [HttpPost]
-    public IActionResult Login(LoginViewModel viewModel)
+    public async Task<IActionResult> Login(LoginViewModel viewModel)
     {
+        if (!ModelState.IsValid) return View(viewModel);
+
+        var user = await _userManager.FindByNameAsync(viewModel.Username);
+        if (user == null)
+        {
+            ModelState.AddModelError(String.Empty, "User not found");
+            return View(viewModel);
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(viewModel.Username, viewModel.Password, false, false);
+        if (result.Succeeded)
+        {
+            return Redirect(viewModel.ReturnUrl);
+        }
+        ModelState.AddModelError(String.Empty, "Login error");
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult Register(string returnUrl)
+    {
+        var viewModel = new RegisterViewModel
+        {
+            ReturnUrl = returnUrl
+        };
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+
+        var user = new AppUser
+        {
+            UserName = viewModel.Username
+        };
+
+        var result = await _userManager.CreateAsync(user, viewModel.Password);
+        if (result.Succeeded)
+        {
+            await _signInManager.SignInAsync(user, false);
+            return Redirect(viewModel.ReturnUrl);
+        }
+        ModelState.AddModelError(String.Empty, "Error occured");
+        return View(viewModel);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Logout(string logoutId)
+    {
+        await _signInManager.SignOutAsync();
+        var logoutRequest = await _interactionService.GetLogoutContextAsync(logoutId);
+        return Redirect(logoutRequest.PostLogoutRedirectUri);
     }
 }
